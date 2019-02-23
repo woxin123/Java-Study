2f2657474f23324cae0d7a94bbe4ca031af50478
woxin <1293141942@qq.com>
2019-02-23 11:46:33 +0800

    享元模式

---
+++ study_java/study-design_patterns/src/main/java/top/mcwebsite/flyweight_pattern/ConcreteFlyweight1.java
@@ -0,0 +1,18 @@
+package top.mcwebsite.flyweight_pattern;
+
+/**
+ * @author mengchen
+ * @time 19-2-22 下午8:20
+ */
+public class ConcreteFlyweight extends Flyweight {
+
+    public ConcreteFlyweight(String extrinsic) {
+        super(extrinsic);
+    }
+
+    // 根据外部状态进行逻辑处理
+    @Override
+    public void operate() {
+        // 业务逻辑
+    }
+}
---
+++ study_java/study-design_patterns/src/main/java/top/mcwebsite/flyweight_pattern/FlyweightFactory.java
@@ -0,0 +1,8 @@
+package top.mcwebsite.flyweight_pattern;
+
+/**
+ * @author mengchen
+ * @time 19-2-22 下午8:22
+ */
+public class FlyweightFactory {
+}
