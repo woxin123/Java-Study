600dc1da515c4fb7b7fefed1b0e5800ad3e1276a
woxin <1293141942@qq.com>
2019-03-27 22:25:40 +0800

    创建关于spring事务项目

---
+++ study_spring/spring-transcational/src/main/java/top/mcwebsite/springtranscational/SpringTranscationalApplication.java
@@ -0,0 +1,13 @@
+package top.mcwebsite.springtranscational;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTranscationalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTranscationalApplication.class, args);
+    }
+
+}
